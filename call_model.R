15dbad1a6658b1563fb0dc357be6017b206c16ad
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-06-11 15:09:24 -0300

    Update call_model.R

--- call_model.R
+++ call_model.R
@@ -1,12 +1,20 @@
 # Após salvar no enviroment os códigos, este script rodará as rotinas
 
+library(tidyverse)
+library(randomForest)
+
 load("data.rda")
+
+# é necessário tranformar a coluna data como nomes das linhas
 
 View(data)
 
 dates = data$date
 data = data%>%select(-date)%>%as.matrix()
 rownames(data) = as.character(dates)
+
+
+
 
 rolling_window(runrf, data, nwindow=2, horizon = 2,variable = "CPIAUCSL", add_dummy = TRUE, nlags=12)
 
