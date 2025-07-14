224ac88a503663a66dbef5d83eae85b83d88c45c
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-07-14 12:55:06 -0300

    Update call_model.R

--- call_model.R
+++ call_model.R
@@ -26,28 +26,25 @@ dates = data$date
 data = data%>%select(-date)%>%as.matrix()
 rownames(data) = as.character(dates)
 
-nwindows = 4
+nwindows = 180
 model_list = list() 
 
 for(i in 1:12){
-  k=0
-  model_list[[i]] = list()
-  for (j in c(4, 12)) {
-  model = rolling_window(runrf,data,nwindows+i-1,i,"CPIAUCSL", add_dummy = TRUE, nlags=j)    # 
-  model_list[[i]][[k=k+1]] = model                                                                    # 
+  model = rolling_window(model_function,data,nwindows+i-1,i,"CPIAUCSL")
+  model_list[[i]] = model
   cat(i,"\n")
 }
-}
 
-# revisar o for j, verificar o que fica salvo
+
+#cria matriz forecasts 
 
 forecasts = Reduce(cbind,lapply(model_list, function(x)head(x$forecast,nwindows)))
 
 
 
-
-# o accumulate_model calcula as diagonais, sendo assim, os valores de previsao de 3 e 6 me
+# o accumulate_model calcula as diagonais, sendo assim, os valores de previsao de 3 e 6 meses e adiciona as colunas referetnes
 forecasts = accumulate_model(forecasts)
+
 
 save(forecasts,file = paste("forecasts/",model_name,".rda",sep = ""))
 
