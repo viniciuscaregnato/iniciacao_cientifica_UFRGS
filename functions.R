462afae740ec9e395d5c9e9060fed6786af05fde
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-07-18 11:15:52 -0300

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,21 +1,11 @@
 # dataprep
 
-dataprep = function(ind, df, variable, horizon = horizon, add_dummy = TRUE, univar = FALSE, factonly = FALSE, nofact = FALSE, nlags=nlags)
-  
-  # ind: indices das linhas que serao usadadas do dataframe ao aplicar df=df[ind,]
-  # df: um data.frame
-  # variable: nome da variavel do dataframe a ser analisada, será o y
-  # horizon: horizonte de previsao, que determina quantas últimas observações serão retiradas da matriz X ao criar Xin e Xout
-  # add_dummy (padrao TRUE): adiciona coluna "dummy" para a variavel "2008-11-01" na matriz "Xin" e uma coluna de zeros à matriz "Xout"
-  # univar (padrao FALSE): caso for TRUE, os dados df[,variabele] sao transformados na matriz X, tornado a serie apenas determinada pelas suas defasagens
-  #                        caso for FALSE, dai depende de factonly e nofact
-  # factonly (padrao FALSE): só existe se nofactor for FALSE, caso for TRUE, é usado apenas fatores(primeiros 4 componentes principais) como variaveis
-  # nofact (padrao FALSE): se for TRUE, ignora a extração de componentes pricipais PCA, e usa apenas os dados do modelo
+dataprep = function(ind, df, variable, horizon = horizon, add_dummy = TRUE, univar = FALSE, factonly=FALSE, nofact = FALSE, nlags=nlags)
   
 {
   
-  df=df[ind,]                                        # define as linhas de df conformeo indice indicado pela coluna de indamt
-  y=df[,variable]                                    # separa coluna de y neste df do momento
+  df=df[ind,]
+  y=df[,variable]                                    
   
   
   if (nofact==TRUE){                                # nao se observa NOFACT = TRUE no meu algoritmo
@@ -32,39 +22,39 @@ dataprep = function(ind, df, variable, horizon = horizon, add_dummy = TRUE, univ
     
     if(univar==FALSE){
       factors=princomp(scale(df))$scores[,1:4]
-      x=cbind(df,factors)                            # se multivar + factor, default adiciona principal components na matriz X
+      x=cbind(df,factors)                            
     }else{
-      x = as.matrix(df[,variable])                   # se univar  + factor, matriz X é apenas de target var
+      x = as.matrix(df[,variable])                   
     }
     
   }
   
   
   
-  X=embed(as.matrix(x),nlags)                        # X é a matriz x, com nlags defasagens
+  X=embed(as.matrix(x),nlags)                        
   
-  Xin=X[-c((nrow(X)-horizon+1):nrow(X)),]            # remove as ultimas horizon linhas de X
-  Xout=X[nrow(X),]                                   # armazena em Xout os valores da ultima linha da matriz X, em forma de vetor
-  Xout=t(as.vector(Xout))                            # garante que Xout seja de fato uma linha, n apenas um vetor
-  yin=tail(y,nrow(Xin))                              # pega os últimos nrow(Xin) valores do vetor y
+  Xin=X[-c((nrow(X)-horizon+1):nrow(X)),]            
+  Xout=X[nrow(X),]                                   
+  Xout=t(as.vector(Xout))                            
+  yin=tail(y,nrow(Xin))                              
   
   
   
-  if("2008-11-01" %in% names(yin)){                  # (se 2008-11-01 está no names(yin):
+  if("2008-11-01" %in% names(yin)){                  
     
-    dummy=rep(0,length(yin))                         # cria um vetor de zeros, chamado dummy, com a extensao de yin
-    intervention=which(names(yin)=="2008-11-01")     # "intervention" apenas guarda o indice de "2008-11-01"
-    dummy[intervention]=1                            # tona o valor, no vetor "dummy", igual a 1, para o indice de internvention
-    if(add_dummy == TRUE){                           # (ainda se, add_dumy == TRUE, 
-      Xin=cbind(Xin,dummy)                           # adiciona coluna "dummy" à matriz "Xin"
-      Xout=cbind(Xout,0)                             # adiciona uma coluna de zeros à matriz "Xout"))
+    dummy=rep(0,length(yin))                         
+    intervention=which(names(yin)=="2008-11-01")     
+    dummy[intervention]=1                            
+    if(add_dummy == TRUE){                            
+      Xin=cbind(Xin,dummy)                           
+      Xout=cbind(Xout,0)                             
     }
     
-  }else{                                             # (se 2008-11-01 nao está no names(yin):
-    dummy = rep(0,length(yin))                       # cria um vetor de zeros, chamado dummy, com a extensao de yin
-    if(add_dummy == TRUE){                           # (ainda, se add_dummy for TRUE na função:
-      Xin=cbind(Xin,dummy)                           # adiciona coluna "dummy" à matriz "Xin"
-      Xout=cbind(Xout,0)                             # adiciona uma coluna de zeros à matriz "Xout"))
+  }else{                                             
+    dummy = rep(0,length(yin))                       
+    if(add_dummy == TRUE){                           
+      Xin=cbind(Xin,dummy)                           
+      Xout=cbind(Xout,0)                             
     }
   }
   
@@ -75,20 +65,17 @@ dataprep = function(ind, df, variable, horizon = horizon, add_dummy = TRUE, univ
 
 # autoregressive runar ####
 
-runar=function(ind,df,variable = variable,horizon = horizon, type = "fixed", nlags=nlags){
+runar=function(ind,df,variable = variable,horizon = horizon, type = "bic", nlags=nlags){
   prep_data = dataprep(ind,df,variable,horizon, add_dummy = FALSE, univar = TRUE, nlags = nlags)
   Xin = prep_data$Xin
   yin = prep_data$yin
   Xout = prep_data$Xout
   dummy = prep_data$dummy
   
-  # Xin é constituído apenas por "variable", embedded nlags vezes
-  # nao é adicionado vetor de dummy na matriz X, apenas criado, se 2008-11-01 estiver no names(yin)
-  # o Xout sao as ultimas observações da janela X, que coicidem com a ultima linha de Yin
   
   if(type=="fixed"){
     modelest=lm(yin~Xin+dummy)
-    best = ncol(Xin)                        # best é o numero de colunas de Xin
+    best = ncol(Xin)                        
   }
   
   if(type=="bic"){
@@ -104,10 +91,9 @@ runar=function(ind,df,variable = variable,horizon = horizon, type = "fixed", nla
       }
     }
   }
-  coef=coef(modelest)                           # pega os coeficientes estimados na regressao 
-  coef[is.na(coef)] = 0                         # coeficientes com valor NA recebem o valor zero
-  # dummy pode ficar NA por multicolinearidade
-  forecast=c(1,Xout[,1:best],0)%*%coef          # atualiza Xout para c(1,Xout[,1:best],0) e multiplica pelos coeficientes estiamdos     
+  coef=coef(modelest)                            
+  coef[is.na(coef)] = 0                         
+  forecast=c(1,Xout[,1:best],0)%*%coef               
   
   return(list(forecast=forecast))
 }
@@ -117,32 +103,25 @@ runar=function(ind,df,variable = variable,horizon = horizon, type = "fixed", nla
 
 runrf=function(ind,df,variable,horizon, nlags = nlags)
   
-  # ind: indices das linhas q serao usadas, recebido pelo apply, que extraiu da indmat
-  # df: o dataframe
-  # variable: a variavel dependente
-  # horizon: o horizonte de previsao
-  
+
 {
-  prep_data = dataprep(ind,df,variable,horizon, nlags = nlags)     # roda o dataprep
-  Xin = prep_data$Xin                               # chama o Xin de dataprep
-  yin = prep_data$yin                               # chama o yin de dataprep
-  Xout = prep_data$Xout                             # chama o Xout de dataprep
+  prep_data = dataprep(ind,df,variable,horizon, nlags = nlags)     
+  Xin = prep_data$Xin                               
+  yin = prep_data$yin                               
+  Xout = prep_data$Xout                             
   
-  modelest=randomForest::randomForest(Xin,yin, importance = TRUE)  # modelest = "modelo estimado"
-  # randomForest::randomForest() chama a função randomForest() sem chamar library(randomForest) 
-  # importance salva as variaveis mais importantes atraves de analise de erro OOB e permuting variables
+  modelest=randomForest::randomForest(Xin,yin, importance = TRUE)
   
-  forecast=predict(modelest,Xout)                   # aplica modelest aos dados Xout        
+  
+  forecast=predict(modelest,Xout)                           
   
   ## outputs
-  importance = randomForest::importance(modelest)   # acessa as variaveis mais importantes de modelest 
-  outputs = list(importance = importance)           # lista as variaveis mais importantes
+  importance = randomForest::importance(modelest)    
+  outputs = list(importance = importance)           
   
   return(list(forecast=forecast, outputs = outputs))
   
-  #retona 1) a previsao e 2) a lista de variaveis mais importantes
 }
-
 
 # accumulate_model ####
 
