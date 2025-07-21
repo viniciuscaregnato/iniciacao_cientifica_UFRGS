34f1ffcb5c9dc7be4301b210f3143da92047c9d3
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-07-21 16:13:52 -0300

    Update and rename evaluate_results.R to evaluate_results_h4.R

--- evaluate_results_h4.R
+++ evaluate_results_h4.R
@@ -3,13 +3,13 @@ library(tidyverse)
 load("forecasts/yout.rda")
 load("forecasts/rw.rda")
 
-model_files = setdiff(list.files("forecasts/"),c("rw.rda","yout.rda"))
+model_files = setdiff(list.files("forecasts_h4/"),c("rw.rda","yout.rda"))
 
 models_list = list()
 for(i in 1:length(model_files)){
   
-  load(paste("forecasts/",model_files[i],sep = ""))
-  models_list[[i]] = forecasts
+  load(paste("forecasts_h4/",model_files[i],sep = ""))
+  models_list[[i]] = forecasts_h4
   
 }
 names(models_list) = model_files
@@ -30,6 +30,6 @@ errorsacc = lapply(models_list, function(x){
 })%>% Reduce(f=cbind)
 colnames(errorsacc) = model_files
 
-res = as.matrix(t(c(errors/rwe,
-            errorsacc/rweacc)))
-View(res)
+res_h4 = as.matrix(t(c(errors/rwe,
+                    errorsacc/rweacc)))
+View(res_h4)
