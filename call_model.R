cbc802606be771efe7ed0aac627c5a3402e63665
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-07-21 15:52:55 -0300

    Update call_model.R

--- call_model.R
+++ call_model.R
@@ -1,7 +1,7 @@
 library(devtools)
 
 
-# install_github("gabrielrvsc/HDeconometrics", force = TRUE)
+# install_github("gabrielrvsc/HDeconometrics", force = TRUE) ### ainda nao entedi o uso
 library(HDeconometrics)
 library(glmnet)
 library(randomForest)
@@ -13,12 +13,12 @@ source("functions/rolling_window.R")
 source("functions/functions.R")
 
 
-model_name <- "AR"
+model_name <- "ARrr"
 model_function <- runar
 
 
 
-load("data.rda")
+load("data/data.rda")
 
 View(data)
 
@@ -27,27 +27,44 @@ data = data%>%select(-date)%>%as.matrix()
 rownames(data) = as.character(dates)
 
 nwindows = 180
+lags_list = list()
 model_list = list() 
 
-for(i in 1:12){
-  model = rolling_window(model_function,data,nwindows+i-1,i,"CPIAUCSL")
-  model_list[[i]] = model
-  cat(i,"\n")
+lags = c(4,12)
+
+
+for (j in lags) {
+  contador <- match(j, lags)
+  for(i in 1:12){
+    model = rolling_window(model_function,data,nwindows+i-1,i,"CPIAUCSL", nlags=j) 
+    model_list[[i]] = model                                                                     
+    cat(i,"\n")
+}
+  lags_list[[contador]] = model_list
+    cat(j, "\n")
 }
 
 
-#cria matriz forecasts 
-
-forecasts = Reduce(cbind,lapply(model_list, function(x)head(x$forecast,nwindows)))
-
+forecasts_h4 = Reduce(cbind, lapply(lags_list[[1]], function(x)head(x$forecast,nwindows)))
+forecasts_h12 = Reduce(cbind,lapply(lags_list[[2]], function(x)head(x$forecast,nwindows)))
 
 
-# o accumulate_model calcula as diagonais, sendo assim, os valores de previsao de 3 e 6 meses e adiciona as colunas referetnes
-forecasts = accumulate_model(forecasts)
+
+# o accumulate_model calcula as diagonais, sendo assim, os valores de previsao de 3 e 6 meses
+forecasts_h4 = accumulate_model(forecasts_h4)
+forecasts_h12 = accumulate_model(forecasts_h12)
 
 
-save(forecasts,file = paste("forecasts/",model_name,".rda",sep = ""))
+View(forecasts_h12)
+View(forecasts_h4)
+
+
+
+save(forecasts_h4,file = paste("forecasts_h4/",model_name,".rda",sep = ""))
+
+save(forecasts_h12,file = paste("forecasts_h12/",model_name,".rda",sep = ""))
 
 
 plot(tail(data[,"CPIAUCSL"],180),type = "l")
-lines(forecasts[,1],col = 3)
+lines(forecasts_h12[,1],col = 3)
+lines(forecasts_h4[,1],col = 4)
