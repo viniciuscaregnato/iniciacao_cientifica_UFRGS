de1031a8abfad18690b5b612a1c10f7ba7656ffd
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-08-16 16:36:25 -0300

    Update eval_csfe_12lags.R

--- eval_csfe_12lags.R
+++ eval_csfe_12lags.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# organizando dados
+
 load("forecasts_12lags/yout.rda")
 load("forecasts_12lags/rw.rda")
 
@@ -15,45 +17,52 @@ for(i in 1:length(model_files)){
 names(models_list) = model_files
 
 # CSFE h=1 ####
-errors_h1 = lapply(models_list, function(x){
+CSFE_h1_12lags = lapply(models_list, function(x){
   cumsum((rw[,1]-yout[,1])^2)-((x[,1]-yout[,1])^2)
 })%>% Reduce(f=cbind)
-colnames(errors_h1) = model_files
+colnames(CSFE_h1_12lags) = model_files
 
-View(errors_h1)
+View(CSFE_h1_12lags)
 
-matplot(1:nrow(errors_h1),  errors_h1, type="l", col=1:4, lty=1,
+matplot(1:nrow(CSFE_h1_12lags),  CSFE_h1_12lags, type="l", col=length(model_files), lty=1,
         xlab="Data", ylab="CSFE_h1_12lags")
 
 # CSFE h=3 ####
-errors_h3 = lapply(models_list, function(x){
+CSFE_h3_12lags = lapply(models_list, function(x){
   cumsum((rw[,3]-yout[,1])^2)-((x[,3]-yout[,1])^2)
 })%>% Reduce(f=cbind)
-colnames(errors_h3) = model_files
+colnames(CSFE_h3_12lags) = model_files
 
-matplot(1:nrow(errors_h3),  errors_h3, type="l", col=1:4, lty=1,
+matplot(1:nrow(CSFE_h3_12lags),  CSFE_h3_12lags, type="l", col=length(model_files), lty=1,
         xlab="Data", ylab="CSFE_h3_12lags")
 
 # CSFE h=6 ####
-errors_h6 = lapply(models_list, function(x){
+CSFE_h6_12lags = lapply(models_list, function(x){
   cumsum((rw[,6]-yout[,1])^2)-((x[,6]-yout[,1])^2)
 })%>% Reduce(f=cbind)
-colnames(errors_h6) = model_files
+colnames(CSFE_h6_12lags) = model_files
 
-matplot(1:nrow(errors_h6),  errors_h6, type="l", col=1:4, lty=1,
+matplot(1:nrow(CSFE_h6_12lags),  CSFE_h6_12lags, type="l", col=length(model_files), lty=1,
         xlab="Data", ylab="CSFE_h6_12lags")
 
 # CSFE h=12 ####
-errors_h12 = lapply(models_list, function(x){
+CSFE_h12_12lags = lapply(models_list, function(x){
   cumsum((rw[,12]-yout[,1])^2)-((x[,12]-yout[,1])^2)
 })%>% Reduce(f=cbind)
-colnames(errors_h12) = model_files
+colnames(CSFE_h12_12lags) = model_files
 
-matplot(1:nrow(errors_h12),  errors_h12, type="l", col=1:4, lty=1,
+matplot(1:nrow(CSFE_h12_12lags),  CSFE_h12_12lags, type="l", col=length(model_files), lty=1,
         xlab="Data", ylab="CSFE_h12_12lags")
 
 
-save(errors_h1,file = "csfe_results/h1_12lags.rda")
-save(errors_h3,file = "csfe_results/h3_12lags.rda")
-save(errors_h6,file = "csfe_results/h6_12lags.rda")
-save(errors_h12,file = "csfe_results/h12_12lags.rda")
+save(CSFE_h1_12lags,file = "csfe_results/h1_12lags.rda")
+save(CSFE_h3_12lags,file = "csfe_results/h3_12lags.rda")
+save(CSFE_h6_12lags,file = "csfe_results/h6_12lags.rda")
+save(CSFE_h12_12lags,file = "csfe_results/h12_12lags.rda")
+
+
+# para visualizar uma curva CSFE ####
+
+load("csfe_results/h1_12lags.rda")
+matplot(1:nrow(CSFE_h1_12lags),  CSFE_h1_12lags, type="l", col=length(model_files), lty=1,
+        xlab="Data", ylab="CSFE_h1_12lags")
