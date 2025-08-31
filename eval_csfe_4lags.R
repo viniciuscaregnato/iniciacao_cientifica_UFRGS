976d72be1459efbc8b494d10e33187d9ee23706b
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-08-31 15:18:19 -0300

    Update eval_csfe_4lags.R

--- eval_csfe_4lags.R
+++ eval_csfe_4lags.R
@@ -1,6 +1,4 @@
-library(tidyverse)
-
-# organizando dados
+# organizando dados ####
 
 load("forecasts_4lags/yout.rda")
 load("forecasts_4lags/rw.rda")
@@ -17,43 +15,74 @@ for(i in 1:length(model_files)){
 names(models_list) = model_files
 
 # CSFE h=1 ####
-CSFE_h1_4lags = lapply(models_list, function(x){
-  cumsum((rw[,1]-yout[,1])^2)-((x[,1]-yout[,1])^2)
-})%>% Reduce(f=cbind)
+CSFE_h1_4lags = Reduce(cbind,lapply(models_list, function(x){
+  cumsum(((rw[,1]-yout[,1])^2)-((x[,1]-yout[,1])^2))
+}))
 colnames(CSFE_h1_4lags) = model_files
 
-View(CSFE_h1_4lags)
+png("C:/Users/celia/Desktop/ECONOMIA UFRGS/1. INICIAÇÃO CIENTIFICA/R/algoritmo0.3/CSFE_results/CSFE_h1_4lags.png",
+    width=1200, height=800, res=150)
 
-matplot(1:nrow(CSFE_h1_4lags),  CSFE_h1_4lags, type="l", col=length(model_files), lty=1,
-        xlab="Data", ylab="CSFE_h1_4lags")
+matplot(1:nrow(CSFE_h1_4lags),  CSFE_h1_4lags, type="l", col=1:length(model_files), lty=1,
+        xlab="Forecast", ylab="CSFE_h1_4lags")
+
+legend("topleft", legend=colnames(CSFE_h1_4lags), col=1:ncol(CSFE_h1_4lags),
+       lty=1, cex=0.50, bty="n")
+
+dev.off()
 
 # CSFE h=3 ####
-CSFE_h3_4lags = lapply(models_list, function(x){
-  cumsum((rw[,3]-yout[,1])^2)-((x[,3]-yout[,1])^2)
-})%>% Reduce(f=cbind)
+CSFE_h3_4lags = Reduce(cbind, lapply(models_list, function(x){
+  cumsum(((rw[,3]-yout[,1])^2)-((x[,3]-yout[,1])^2))
+}))
 colnames(CSFE_h3_4lags) = model_files
 
-matplot(1:nrow(CSFE_h3_4lags),  CSFE_h3_4lags, type="l", col=length(model_files), lty=1,
-        xlab="Data", ylab="CSFE_h3_4lags")
+png("C:/Users/celia/Desktop/ECONOMIA UFRGS/1. INICIAÇÃO CIENTIFICA/R/algoritmo0.3/CSFE_results/CSFE_h3_4lags.png",
+    width=1200, height=800, res=150)
+
+matplot(1:nrow(CSFE_h3_4lags),  CSFE_h3_4lags, type="l", col=1:length(model_files), lty=1,
+        xlab="Forecast", ylab="CSFE_h3_4lags")
+
+legend("topleft", legend=colnames(CSFE_h1_4lags), col=1:ncol(CSFE_h1_4lags),
+       lty=1, cex=0.50, bty="n")
+
+dev.off()
 
 # CSFE h=6 ####
-CSFE_h6_4lags = lapply(models_list, function(x){
-  cumsum((rw[,6]-yout[,1])^2)-((x[,6]-yout[,1])^2)
-})%>% Reduce(f=cbind)
+CSFE_h6_4lags = Reduce(cbind, lapply(models_list, function(x){
+  cumsum(((rw[,6]-yout[,1])^2)-((x[,6]-yout[,1])^2))
+}))
 colnames(CSFE_h6_4lags) = model_files
 
-matplot(1:nrow(CSFE_h6_4lags),  CSFE_h6_4lags, type="l", col=length(model_files), lty=1,
-        xlab="Data", ylab="CSFE_h6_4lags")
+png("C:/Users/celia/Desktop/ECONOMIA UFRGS/1. INICIAÇÃO CIENTIFICA/R/algoritmo0.3/CSFE_results/CSFE_h6_4lags.png",
+    width=1200, height=800, res=150)
+
+matplot(1:nrow(CSFE_h6_4lags),  CSFE_h6_4lags, type="l", col=1:length(model_files), lty=1,
+        xlab="Forecast", ylab="CSFE_h6_4lags")
+
+legend("topleft", legend=colnames(CSFE_h1_4lags), col=1:ncol(CSFE_h1_4lags),
+       lty=1, cex=0.50, bty="n")
+
+dev.off()
 
 # CSFE h=12 ####
-CSFE_h12_4lags = lapply(models_list, function(x){
-  cumsum((rw[,12]-yout[,1])^2)-((x[,12]-yout[,1])^2)
-})%>% Reduce(f=cbind)
+CSFE_h12_4lags = Reduce(cbind, lapply(models_list, function(x){
+  cumsum(((rw[,12]-yout[,1])^2)-((x[,12]-yout[,1])^2))
+}))
 colnames(CSFE_h12_4lags) = model_files
 
-matplot(1:nrow(CSFE_h12_4lags),  CSFE_h12_4lags, type="l", col=length(model_files), lty=1,
-        xlab="Data", ylab="CSFE_h12_4lags")
+png("C:/Users/celia/Desktop/ECONOMIA UFRGS/1. INICIAÇÃO CIENTIFICA/R/algoritmo0.3/CSFE_results/CSFE_h12_4lags.png",
+    width=1200, height=800, res=150)
 
+matplot(1:nrow(CSFE_h12_4lags),  CSFE_h12_4lags, type="l", col=1:length(model_files), lty=1,
+        xlab="Forecast", ylab="CSFE_h12_4lags")
+
+legend("topleft", legend=colnames(CSFE_h1_4lags), col=1:ncol(CSFE_h1_4lags),
+       lty=1, cex=0.50, bty="n")
+
+dev.off()
+
+#salvando ####
 
 save(CSFE_h1_4lags,file = "csfe_results/h1_4lags.rda")
 save(CSFE_h3_4lags,file = "csfe_results/h3_4lags.rda")
@@ -64,5 +93,7 @@ save(CSFE_h12_4lags,file = "csfe_results/h12_4lags.rda")
 # Para visualizar as curvas ####
 
 load("csfe_results/h12_4lags.rda")
-matplot(1:nrow(CSFE_h12_4lags),  CSFE_h12_4lags, type="l", col=length(model_files), lty=1,
-        xlab="Data", ylab="CSFE_h12_4lags")
+matplot(1:nrow(CSFE_h12_4lags),  CSFE_h12_4lags, type="l", col=1:length(model_files), lty=1,
+        xlab="Forecast", ylab="CSFE_h12_4lags")
+legend("topleft", legend=colnames(CSFE_h1_4lags), col=1:ncol(CSFE_h1_4lags),
+       lty=1, cex=0.50, bty="n")
