c4d49434468443df1558cdc9e4a14e832ece128c
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-09-12 14:04:33 -0300

    Update DM_test_12lags.R

--- DM_test_12lags.R
+++ DM_test_12lags.R
@@ -12,7 +12,7 @@ for(i in 1:(length(model_files)-1)){
   models_list[[i]] = forecasts_12lags
   
 }
-models_list[[7]] = rw
+models_list[[(length(models_list)+1)]] = rw
 
 names(models_list) = model_files
 View(models_list)
@@ -40,165 +40,174 @@ save(errors_list_h3_12lags, file="errors_lists_12lags/errors_list_h3.rda")
 save(errors_list_h6_12lags, file="errors_lists_12lags/errors_list_h6.rda")
 save(errors_list_h12_12lags, file="errors_lists_12lags/errors_list_h12.rda")
 
+
+
+
+## observar se os modelos estão corretos, conforme a matriz de erros
+
+
+
+
+
 # RW x AR ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,1], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,1], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,1], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,1], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,1], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,1], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,1], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,1], h = 12, power = 2, alternative = "two.sided")
 
 # RW x CWB ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,2], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,2], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,2], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,2], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,2], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,2], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,2], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,2], h = 12, power = 2, alternative = "two.sided")
 
 # RW x ELASTIC NET ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,3], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,4], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,3], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,4], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,3], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,4], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,3], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,4], h = 12, power = 2, alternative = "two.sided")
 
 
 
 
 # RW x LASSO ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,4], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,5], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,4], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,5], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,4], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,5], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,4], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,5], h = 12, power = 2, alternative = "two.sided")
 
 # RW x RANDOM FOREST ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,5], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,6], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,5], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,6], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,5], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,6], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,5], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,6], h = 12, power = 2, alternative = "two.sided")
 
 # RW x RIDGE ####
 #h=1
-dm.test(errors_list_h1[,7], errors_list_h1[,6], h = 1, power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,8], errors_list_h1_12lags[,7], h = 1, power = 2, alternative = "two.sided")
 
 #h=3
-dm.test(errors_list_h3[,7], errors_list_h3[,6], h = 3, power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,8], errors_list_h3_12lags[,7], h = 3, power = 2, alternative = "two.sided")
 
 #h=6
-dm.test(errors_list_h6[,7], errors_list_h6[,6], h = 6, power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,8], errors_list_h6_12lags[,7], h = 6, power = 2, alternative = "two.sided")
 
 #h=12
-dm.test(errors_list_h12[,7], errors_list_h12[,6], h = 12, power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,8], errors_list_h12_12lags[,7], h = 12, power = 2, alternative = "two.sided")
 
 
 # CWB x LASSO ####
 
 # h=1
-dm.test(errors_list_h1[,2], errors_list_h1[,4], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,2], errors_list_h1_12lags[,5], h=1,  power = 2, alternative = "two.sided")
 
 
 # h=3
-dm.test(errors_list_h3[,2], errors_list_h3[,4], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,2], errors_list_h3_12lags[,5], h=3,  power = 2, alternative = "two.sided")
 
 
 # h=6
-dm.test(errors_list_h6[,2], errors_list_h6[,4], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,2], errors_list_h6_12lags[,5], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,2], errors_list_h12[,4], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,2], errors_list_h12_12lags[,5], h=12,  power = 2, alternative = "two.sided")
 
 
 # CWB X AR ####
 
 # h=1
-dm.test(errors_list_h1[,2], errors_list_h1[,1], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,2], errors_list_h1_12lags[,1], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,2], errors_list_h3[,1], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,2], errors_list_h3_12lags[,1], h=3,  power = 2, alternative = "two.sided")
 
 
 # h=6
-dm.test(errors_list_h6[,2], errors_list_h6[,1], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,2], errors_list_h6_12lags[,1], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,2], errors_list_h12[,1], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,2], errors_list_h12_12lags[,1], h=12,  power = 2, alternative = "two.sided")
 
 
 # CWB x ELASTIC ####
 
 # h=1
-dm.test(errors_list_h1[,2], errors_list_h1[,3], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,2], errors_list_h1_12lags[,4], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,2], errors_list_h3[,3], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,2], errors_list_h3_12lags[,4], h=3,  power = 2, alternative = "two.sided")
 
 
 # h=6
-dm.test(errors_list_h6[,2], errors_list_h6[,3], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,2], errors_list_h6_12lags[,4], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,2], errors_list_h12[,3], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,2], errors_list_h12_12lags[,4], h=12,  power = 2, alternative = "two.sided")
 
 
 # CWB x RANDOM FOREST ####
 
 # h=1
-dm.test(errors_list_h1[,2], errors_list_h1[,5], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,2], errors_list_h1_12lags[,6], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,2], errors_list_h3[,5], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,2], errors_list_h3_12lags[,6], h=3,  power = 2, alternative = "two.sided")
 
 # h=6
-dm.test(errors_list_h6[,2], errors_list_h6[,5], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,2], errors_list_h6_12lags[,6], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,2], errors_list_h12[,5], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,2], errors_list_h12_12lags[,6], h=12,  power = 2, alternative = "two.sided")
 
 
 # CWB x RIDGE ####
 
 # h=1
-dm.test(errors_list_h1[,2], errors_list_h1[,6], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,2], errors_list_h1_12lags[,7], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,2], errors_list_h3[,6], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,2], errors_list_h3_12lags[,7], h=3,  power = 2, alternative = "two.sided")
 
 # h=6
-dm.test(errors_list_h6[,2], errors_list_h6[,6], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,2], errors_list_h6_12lags[,7], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,2], errors_list_h12[,6], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,2], errors_list_h12_12lags[,7], h=12,  power = 2, alternative = "two.sided")
 
 
 
@@ -209,63 +218,61 @@ dm.test(errors_list_h12[,2], errors_list_h12[,6], h=12,  power = 2, alternative 
 # RANDOM FOREST x AR ####
 
 # h=1
-dm.test(errors_list_h1[,5], errors_list_h1[,1], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,6], errors_list_h1_12lags[,1], h=1,  power = 2, alternative = "two.sided")
 
 
 # h=3
-dm.test(errors_list_h3[,5], errors_list_h3[,1], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,6], errors_list_h3_12lags[,1], h=3,  power = 2, alternative = "two.sided")
 
 # h=6
-dm.test(errors_list_h6[,5], errors_list_h6[,1], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,6], errors_list_h6_12lags[,1], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,5], errors_list_h12[,1], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,6], errors_list_h12_12lags[,1], h=12,  power = 2, alternative = "two.sided")
 
 
 # RANDOM FOREST x ELASTIC NET ####
 
 # h=1
-dm.test(errors_list_h1[,5], errors_list_h1[,3], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,6], errors_list_h1_12lags[,4], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,5], errors_list_h3[,3], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,6], errors_list_h3_12lags[,4], h=3,  power = 2, alternative = "two.sided")
 
 # h=6
-dm.test(errors_list_h6[,5], errors_list_h6[,3], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,6], errors_list_h6_12lags[,4], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,5], errors_list_h12[,3], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,6], errors_list_h12_12lags[,4], h=12,  power = 2, alternative = "two.sided")
 
 # RANDOM FOREST x LASSO ####
 
 # h=1
-dm.test(errors_list_h1[,5], errors_list_h1[,4], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,6], errors_list_h1_12lags[,5], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,5], errors_list_h3[,4], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,6], errors_list_h3_12lags[,5], h=3,  power = 2, alternative = "two.sided")
 
 # h=6
-dm.test(errors_list_h6[,5], errors_list_h6[,4], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,6], errors_list_h6_12lags[,5], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,5], errors_list_h12[,4], h=12,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h12_12lags[,6], errors_list_h12_12lags[,5], h=12,  power = 2, alternative = "two.sided")
 
 
 
 # RANDOM FOREST x RIDGE ####
 
 # h=1
-dm.test(errors_list_h1[,5], errors_list_h1[,6], h=1,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h1_12lags[,6], errors_list_h1_12lags[,7], h=1,  power = 2, alternative = "two.sided")
 
 # h=3
-dm.test(errors_list_h3[,5], errors_list_h3[,6], h=3,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h3_12lags[,6], errors_list_h3_12lags[,7], h=3,  power = 2, alternative = "two.sided")
 
 
 # h=6
-dm.test(errors_list_h6[,5], errors_list_h6[,6], h=6,  power = 2, alternative = "two.sided")
+dm.test(errors_list_h6_12lags[,6], errors_list_h6_12lags[,7], h=6,  power = 2, alternative = "two.sided")
 
 # h=12
-dm.test(errors_list_h12[,5], errors_list_h12[,6], h=12,  power = 2, alternative = "two.sided")
-
-
+dm.test(errors_list_h12_12lags[,6], errors_list_h12_12lags[,7], h=12,  power = 2, alternative = "two.sided")
 
